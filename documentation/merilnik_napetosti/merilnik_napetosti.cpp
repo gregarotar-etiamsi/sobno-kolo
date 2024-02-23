f60248b9029c98ae0e9917dbfbdc2fd07b55dc15
Grega Rotar <grega.rotar@grega.ga>
2024-02-23 21:39:56 +0100

    dodan opis

--- documentation/merilnik_napetosti/merilnik_napetosti.cpp
+++ documentation/merilnik_napetosti/merilnik_napetosti.cpp
@@ -15,38 +15,41 @@ void setup()
 }
 
 double napetost;
-void loop() {
+void loop()
+{
     // pridobitev napetosti
     napetost = pridobiIzhodnoNapetost();
     // izpis napetosti prek serial komunikacije
     Serial.println(napetost);
     // program počaka 1 sekundo (1000ms)
     delay(1000);
-
 }
-
 
 // pridobitev izhodne napetosti
 double pridobiIzhodnoNapetost()
 {
+    // napetost na ESP32 pinu z upoštevanjem odstopanj
+    // pridobiNapetostNaPin --> glej poglavje Programske funkcije za branje analognih vhodov
     double pinVoltage = pridobiNapetostNaPin(NAPETOST_PIN);
+    // dejanska napetost brez delinika napetosti
+    // pretvoriVDejanskoNapetost --> glej poglavje Programske funkcije za branje analognih vhodov
     return pretvoriVDejanskoNapetost(pinVoltage, NAPETOST_UPOR_REFERENCA, NAPETOST_UPOR_MERITEV);
 }
 
 // pridobitev napetosti na analognem pinu
 double pridobiNapetostNaPin(int pin)
 {
-  // analogRead vrne 12 bitno število
-  int analog = analogRead(pin);
-  double napetost = (3.3 / 4095) * analog;
-  return napetost;
+    // analogRead vrne 12 bitno število
+    int analog = analogRead(pin);
+    double napetost = (3.3 / 4095) * analog;
+    return napetost;
 }
 
 // pretvori napetost iz delilnika napetosti v dejansko napetost
 double pretvoriVDejanskoNapetost(double napetost, double uporReferenca, double uporMeritev)
 {
-  // uporReferenca predstavlja referenci upor, na katerem ne merimo napetosti
-  // uporMeritev predstavlja upor, na katerem merimo napetost
-  double dejanskaNapetost = napetost * (1 + (uporReferenca / uporMeritev));
-  return dejanskaNapetost;
+    // uporReferenca predstavlja referenci upor, na katerem ne merimo napetosti
+    // uporMeritev predstavlja upor, na katerem merimo napetost
+    double dejanskaNapetost = napetost * (1 + (uporReferenca / uporMeritev));
+    return dejanskaNapetost;
 }
