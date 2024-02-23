c2450c3d4eca7a65832cc3066357d36119d8e156
Grega Rotar <grega.rotar@grega.ga>
2024-02-23 13:32:55 +0100

    formatirana koda

--- documentation/acs712/acs712.cpp
+++ documentation/acs712/acs712.cpp
@@ -11,24 +11,26 @@
 #define TOK_UPOR_MERITEV 20000
 #define TOK_UPOR_REFERENCA 10000
 
-void setup() {
-  	// omogoči izpis vrednosti preko serial komunikacie
-  	Serial.begin(9600);
-  	// nastavi pin za tok na input
-	pinMode(TOK_PIN, INPUT);
+void setup()
+{
+  // omogoči izpis vrednosti preko serial komunikacie
+  Serial.begin(9600);
+  // nastavi pin za tok na input
+  pinMode(TOK_PIN, INPUT);
 }
 
 double tok;
-void loop() {
-  tok = pridobiIzhodniTok();  
+void loop()
+{
+  tok = pridobiIzhodniTok();
   // izpis toka prek serial komunikacije
-  Serial.println(tok);	
+  Serial.println(tok);
   // program počaka 1 sekundo (1000ms)
   delay(1000);
 }
 
-
-double pridobiIzhodniTok() {
+double pridobiIzhodniTok()
+{
   // napetost na ESP32 pinu z upoštevanjem odstopanj
   // pridobiNapetostNaPin --> glej poglavje funkcije
   double napetostNaPin = pridobiNapetostNaPin(TOK_PIN) - TOK_NAPETOST_ODSTOPANJE;
@@ -40,16 +42,17 @@ double pridobiIzhodniTok() {
   return tok;
 }
 
-
 // pridobitev napetosti na analognem pinu
-double pridobiNapetostNaPin(int pin) {
+double pridobiNapetostNaPin(int pin)
+{
   int analog = analogRead(pin);
   double napetost = (3.3 / 4095) * analog;
   return napetost;
 }
 
 // pretvori napetost iz delilnika napetosti v dejansko napetost
-double pretvoriVDejanskoNapetost(double napetost, double uporReferenca, double uporMeritev) {
+double pretvoriVDejanskoNapetost(double napetost, double uporReferenca, double uporMeritev)
+{
   double dejanskaNapetost = napetost * (1 + (uporReferenca / uporMeritev));
   return dejanskaNapetost;
 }
